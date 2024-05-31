f6d0bbac3d3f03505c1898ff7ee7cf56492bc376
E-Prasetyo <tarsisiusekoprasetyo11@gmail.com>
2024-05-31 23:21:04 +0700

    first commit

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,6 @@
 'use client'
-import logos from '../../assets/images/profile.jpg';
-import bg_home from '../../assets/images/background_home.jpg';
+import logos from '../../../images/profile.jpg';
+import bg_home from '../../../images/background_home.jpg';
 import Image from "next/image";
 import { FaArrowDown } from 'react-icons/fa';
 import { useCallback, useEffect, useState } from 'react';
